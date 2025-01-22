b7e40ea79f109c5add062d60f73c035d01666ef6
leriksss <151545647+leriksss@users.noreply.github.com>
2025-01-22 18:14:35 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,16 @@
+let btn = document.querySelector('.btn')
+let age_input = document.querySelector('.age-input')
+let message = document.querySelector('.message')
+
+btn.addEventListener('click', function() {
+    let age = +age_input.value
+    if (age > 16) {
+        message.innerHTML = 'Вік підходить!'
+        message.style.color = 'green'
+     }else {
+        message.innerHTML = 'Треба бути старшим за 16 років :('
+        message.style.color = 'red'
+    }
+
+})
 
